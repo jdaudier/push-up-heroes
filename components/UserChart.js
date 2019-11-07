cdf0349cc9fecbf220c7497d75545e1e27eea2ca
Joanne Daudier <joanne.daudier@zapier.com>
2019-11-07 05:26:05 -0500

    Add avg number to user chart

--- components/UserChart.js
+++ components/UserChart.js
@@ -49,15 +49,29 @@ const CustomTooltip = ({active, payload, label}) => {
     return null;
 };
 
-const CustomLabel = ({viewBox}) => {
-    const x = viewBox.width + viewBox.x + 30;
-    const y = viewBox.y + 5;
+const CustomLabel = ({viewBox, dailyAvg}) => {
+    const x = viewBox.width + viewBox.x + 35;
+    const y = viewBox.y + 4;
+
+    const xMap = {
+        1: x - 8,
+        2: x - 4,
+        3: x,
+    };
+
+    const avgX = xMap[dailyAvg.toString().length];
+
     return (
-        <text x={x} y={y} textAnchor="end" fill={YELLOW} fontWeight="bold">Avg</text>
+        <g>
+            <text x={avgX} y={y - 16} textAnchor="end" fill={YELLOW} fontWeight="bold">{dailyAvg}</text>
+            <text x={x} y={y} textAnchor="end" fill={YELLOW} fontWeight="bold">avg</text>
+        </g>
     )
 };
 
 const UserChart = ({data, dailyAvg}) => {
+    const shouldShowAvg = data.length > 3;
+
     return (
         <>
             <Header as='h2'
@@ -77,13 +91,13 @@ const UserChart = ({data, dailyAvg}) => {
                 marginBottom: 64
             }}>
                 <ResponsiveContainer width="100%" height={400}>
-                    <BarChart data={data} margin={{right: 42}}>
+                    <BarChart data={data} margin={{right: shouldShowAvg ? 50 : 42}}>
                         <CartesianGrid strokeDasharray="3 3"/>
                         <XAxis dataKey="label" height={70} tick={<CustomXAxisTick/>}/>
                         <YAxis allowDecimals={false} tick={<CustomYAxisTick />}/>
                         <Tooltip content={CustomTooltip} cursor={{fill: 'rgba(185, 192, 201, .3)'}} />
                         <Bar dataKey="value" fill={BLUE} maxBarSize={100} />
-                        {data.length > 3 && <ReferenceLine y={dailyAvg} label={<CustomLabel />} stroke={YELLOW} strokeWidth={2} strokeDasharray="8" />}
+                        {shouldShowAvg && <ReferenceLine y={dailyAvg} label={<CustomLabel dailyAvg={dailyAvg} />} stroke={YELLOW} strokeWidth={2} strokeDasharray="8" />}
                     </BarChart>
 
                 </ResponsiveContainer>
