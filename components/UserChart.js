d5da05e592498e708f035d1a15be56eebd366790
Joanne Daudier <joanne.daudier@zapier.com>
2019-11-07 04:37:42 -0500

    Update user chart tooltip to black background

--- components/UserChart.js
+++ components/UserChart.js
@@ -27,12 +27,19 @@ const CustomTooltip = ({active, payload, label}) => {
 
         if (count > 0) {
             return (
-                <Segment compact raised css={{lineHeight: 1.4, textAlign: 'center'}}>
+                <div css={{
+                    background: 'rgb(53, 60, 72)',
+                    borderRadius: '.28571429rem',
+                    color: 'white',
+                    lineHeight: 1.4,
+                    textAlign: 'center',
+                    padding: 10,
+                }}>
                     <time css={{display: 'block', fontSize: 16}}>{label}</time>
                     <div css={{color: BLUE, fontSize: 25, fontWeight: 900}}>
                         {count}
                     </div>
-                </Segment>
+                </div>
             );
         }
 
