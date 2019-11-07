01e7de2e9aafadede4400f4b850423dc49d84a3d
Joanne Daudier <joanne.daudier@zapier.com>
2019-11-07 01:54:10 -0500

    Fix bug where text in user chart was cut off

--- components/UserChart.js
+++ components/UserChart.js
@@ -72,7 +72,7 @@ const UserChart = ({data, dailyAvg}) => {
                 <ResponsiveContainer width="100%" height={400}>
                     <BarChart data={data} margin={{right: 42}}>
                         <CartesianGrid strokeDasharray="3 3"/>
-                        <XAxis dataKey="label" height={60} tick={<CustomXAxisTick/>}/>
+                        <XAxis dataKey="label" height={70} tick={<CustomXAxisTick/>}/>
                         <YAxis allowDecimals={false} tick={<CustomYAxisTick />}/>
                         <Tooltip content={CustomTooltip} cursor={{fill: 'rgba(185, 192, 201, .3)'}} />
                         <Bar dataKey="value" fill={BLUE} maxBarSize={100} />
