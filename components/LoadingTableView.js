2103e5e85f304082140972bd0227f05898de4074
Joanne Daudier <joanne.daudier@zapier.com>
2019-11-08 10:30:11 -0500

    Remove quotes from LoadingTableView

--- components/LoadingTableView.js
+++ components/LoadingTableView.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Dimmer, Loader, Table } from 'semantic-ui-react';
-import quote from '../utils/randomQuotes';
 import { jsx } from '@emotion/core';
 /** @jsx jsx */
 
@@ -11,26 +10,10 @@ const LoadingTableView = () => {
                 <Table.Cell colSpan={6}>
                     <Dimmer active inverted >
                         <Loader size='massive'>
-                            <blockquote css={{
-                                fontStyle: 'italic',
-                                lineHeight: 1.4,
-                                marginBottom: '.5em',
-                                marginTop: '.4em',
-                                '@media(max-width: 767px)': {
-                                    width: '70vw'
-                                }
-                            }}>
-                                &ldquo;{quote.text}&rdquo;
-                            </blockquote>
-                            <small css={{
-                                color: 'rgba(0, 0, 0, .4)',
-                            }}>&ndash; {quote.author}</small>
+                            Loading
                         </Loader>
                     </Dimmer>
                 </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-                <Table.Cell colSpan={6} />
             </Table.Row>
             <Table.Row>
                 <Table.Cell colSpan={6} />
