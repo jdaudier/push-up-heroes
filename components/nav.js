db5421313abb5379b348b413766d11096e665a2d
Joanne Daudier <joanne.daudier@zapier.com>
2021-10-26 17:54:54 -0700

    Add /instructions message

--- components/nav.js
+++ components/nav.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
-import { Header, Menu, Container } from 'semantic-ui-react';
+import {Header, Menu, Container} from 'semantic-ui-react';
 import SuperGirl from './SuperGirl';
+import {useRouter} from "next/router";
 
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 
 const Nav = () => {
+    const router = useRouter();
+    const id = router.query.id;
+
     return (
         <Menu fixed='top' className="top-nav" size="huge">
             <Container>
@@ -20,6 +24,25 @@ const Nav = () => {
                         </Header>
                     </Menu.Item>
                 </Link>
+                {!id && (
+                    <div css={{
+                        alignSelf: 'center',
+                        color: 'rgba(0,0,0,.6)',
+                        fontSize: '.9em',
+                        marginLeft: 'auto'
+                    }}>
+                        Type
+                        <code css={{
+                            backgroundColor: 'rgba(27,31,35,.05)',
+                            borderRadius: 3,
+                            fontSize: '95%',
+                            padding: '.2em .4em',
+                            marginLeft: 4,
+                            marginRight: 4,
+                        }}>/instructions</code>
+                        {" "}in Slack to get started
+                    </div>
+                )}
             </Container>
         </Menu>
     );
