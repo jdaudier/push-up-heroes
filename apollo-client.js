108ec3e25d7a43ad3a42cac16d7b133c743bb748
Joanne Daudier <joanne.daudier@zapier.com>
2021-10-27 12:21:06 -0700

    Adjust caching options

--- apollo-client.js
+++ apollo-client.js
@@ -4,6 +4,11 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
     uri: "https://pushupheroes.com/api/graphql",
     cache: new InMemoryCache(),
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'cache-and-network',
+        },
+    },
 });
 
 export default client;
