eb0f6367451809d9955675dabc203cffa3ee221c
Joanne Daudier <joanne.daudier@zapier.com>
2021-10-31 14:19:42 -0700

    Add "text" to challenge (better var name)

--- pages/api/challenge.js
+++ pages/api/challenge.js
@@ -39,7 +39,7 @@ async function handler(req, res) {
         const count = Number(stringCount);
         const pushUps = count === 1 ? 'push-up' : 'push-ups';
         const context = "_You too can challenge someone with the `/challenge` command._";
-        const text =  `<@${user_id}> just challenged ${recipientName} to *${count}* ${pushUps}!\n*Do you accept this challenge ${recipientName}?*\n\n_If you accept, *${count}* ${pushUps} will be logged for you_.`;
+        const reply = `<@${user_id}> just challenged ${recipientName} to *${count}* ${pushUps}!\n*Do you accept this challenge ${recipientName}?*\n\n_If you accept, *${count}* ${pushUps} will be logged for you_.`;
 
         try {
             await fetch(slackPostMessageUrl, {
@@ -50,7 +50,7 @@ async function handler(req, res) {
                 },
                 body: JSON.stringify({
                     channel: 'fun-push-up-challenge',
-                    text,
+                    text: reply,
                     blocks: [
                         {
                             "type": "section",
@@ -63,7 +63,7 @@ async function handler(req, res) {
                             "type": "section",
                             "text": {
                                 "type": "mrkdwn",
-                                text
+                                text: reply
                             },
                             "accessory": {
                                 "type": "image",
