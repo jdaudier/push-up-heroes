65f1ae181d98f5afc22926e4cfa9643ab57f1346
Joanne Daudier <joanne.daudier@zapier.com>
2021-10-31 13:56:52 -0700

    Fix "This content can't be displayed" bug in Slack

--- pages/api/pushups.js
+++ pages/api/pushups.js
@@ -65,13 +65,14 @@ async function handler(req, res) {
             async function postToChannelForFirstEntry() {
                 const pushUps = count === 1 ? 'push-up' : 'push-ups';
                 const context = "_Use the `/pushups` command to log your set._";
+                const text = `<@${user_id}> just did *${text}* ${pushUps}! :muscle:\n:party: This is the first set to be logged! You're a pioneer!\n\n${context}`;
 
                 try {
                     const blocks = [{
                         "type": "section",
                         "text": {
                             "type": "mrkdwn",
-                            "text": `<@${user_id}> just did *${text}* ${pushUps}! :muscle:\n:party: This is the first set to be logged! You're a pioneer!\n\n${context}`
+                            text,
                         }
                     }];
 
@@ -83,6 +84,7 @@ async function handler(req, res) {
                         },
                         body: JSON.stringify({
                             channel: 'fun-push-up-challenge',
+                            text,
                             blocks,
                         })
                     });
@@ -125,12 +127,13 @@ async function handler(req, res) {
                 try {
                     const {blocks: myStatsBlocks, rawStats} = await getMyStats(user_id, {tagUser: true});
                     const smartResponse = getSmartResponse({id: user_id, count, ...rawStats});
+                    const text = `<@${user_id}> just did *${text}* ${pushUps}! :muscle:\n${smartResponse}\n${context}`;
 
                     const blocks = [{
                         "type": "section",
                         "text": {
                             "type": "mrkdwn",
-                            "text": `<@${user_id}> just did *${text}* ${pushUps}! :muscle:\n${smartResponse}\n${context}`,
+                            text,
                         }
                     }];
 
@@ -142,15 +145,19 @@ async function handler(req, res) {
                         },
                         body: JSON.stringify({
                             channel: 'fun-push-up-challenge',
+                            text,
                             blocks,
                         })
                     });
 
                     const {channel, ts} = await response.json();
 
+                    const fallbackText = myStatsBlocks[0].text.text;
+
                     const myStatsResponse = {
                         channel,
                         thread_ts: ts,
+                        text: fallbackText,
                         blocks: myStatsBlocks,
                     };
 
