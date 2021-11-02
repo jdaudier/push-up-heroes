57e48a01f9175b675c769bc0ecf3608f8de9f719
Joanne Daudier <joanne.daudier@zapier.com>
2021-11-02 02:47:32 -0700

    Add date & time disclaimer

--- components/GlobalFeed.js
+++ components/GlobalFeed.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { useQuery, gql } from '@apollo/client';
-import { Table, Image, Icon } from 'semantic-ui-react';
+import { Table, Image, Icon, Popup } from 'semantic-ui-react';
 import { BLUE, FEED_LIMIT } from '../utils/constants';
 import LoadingTableView from './LoadingTableView';
 import FeedPagination from './FeedPagination';
@@ -34,6 +34,10 @@ const linkCss = {
     cursor: 'pointer',
     color: 'initial',
 };
+
+const dateDisclaimer = {
+    textAlign: 'right'
+}
 
 function MaybeLink({className, rowSpan, shouldCellBeHidden, slackId, realName, children}) {
     const cellCss = {
@@ -85,6 +89,10 @@ function MaybeLink({className, rowSpan, shouldCellBeHidden, slackId, realName, c
     );
 }
 
+const DateDisclaimerTooltip = ({trigger}) => (
+    <Popup content="In each athlete's local time" position='top left' trigger={trigger} />
+)
+
 const GlobalFeed = ({totalSets, totalPushUps, bestIndividualSetCount}) => {
     const [activePage, setActivePage] = useState(1);
     const { loading, error, data } = useQuery(GET_GLOBAL_USERS_FEED, {
@@ -94,171 +102,191 @@ const GlobalFeed = ({totalSets, totalPushUps, bestIndividualSetCount}) => {
     const totalPages = Math.ceil(totalSets / FEED_LIMIT);
 
     return (
-        <Table celled padded selectable size='large' striped textAlign="left">
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell className="mobile-hidden" width={1}>Sets
-                        <span css={{color: BLUE, marginLeft: 6}}>
-                            ({totalSets.toLocaleString()})
-                        </span>
-                    </Table.HeaderCell>
-                    <Table.HeaderCell>Day</Table.HeaderCell>
-                    <Table.HeaderCell>Date</Table.HeaderCell>
-                    <Table.HeaderCell>Time</Table.HeaderCell>
-                    <Table.HeaderCell>Athlete</Table.HeaderCell>
-                    <Table.HeaderCell width={2}>Count
-                        <span css={{color: BLUE, marginLeft: 6}}>
-                            ({totalPushUps.toLocaleString()})
-                        </span>
-                    </Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-
-            {!data ? <LoadingTableView /> : (
-                <Table.Body>
-                    {data.globalUsersFeed.feed.map(({slackId, dayOfWeek, date, time, count, simplifiedDate, profile}, i, arr) => {
-                        const rowSpan = data.globalUsersFeed.setsByDayMap[simplifiedDate];
-
-                        const firstIndex = arr.findIndex(item => item.simplifiedDate === simplifiedDate);
-
-                        const shouldCellBeHidden = rowSpan > 1 && i > firstIndex;
-
-                        const maybeSetsCell = shouldCellBeHidden ? null : (
-                            <MaybeLink className="mobile-hidden"
-                                       realName={profile.real_name}
-                                       rowSpan={rowSpan}
-                                       slackId={slackId}>
-                                {rowSpan}
-                            </MaybeLink>
-                        );
-
-                        const maybeDayCell = (
-                            <MaybeLink rowSpan={rowSpan}
-                                       realName={profile.real_name}
-                                       shouldCellBeHidden={shouldCellBeHidden}
-                                       slackId={slackId}>
-                                {dayOfWeek}
-                            </MaybeLink>
-                        );
-
-                        const maybeDateCell = (
-                            <MaybeLink rowSpan={rowSpan}
-                                       realName={profile.real_name}
-                                       shouldCellBeHidden={shouldCellBeHidden}
-                                       slackId={slackId}>
-                                {date}
-                            </MaybeLink>
-                        );
-
-                        return (
-                            <Table.Row key={i}>
-                                {maybeSetsCell}
-                                {maybeDayCell}
-                                {maybeDateCell}
-                                <Table.Cell>
-                                    <Link href='/users/[id]' as={`/users/${slackId}`}>
-                                        <a title={`${profile.real_name}'s page`} css={linkCss}>
-                                            {time}
-                                        </a>
-                                    </Link>
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Link href='/users/[id]' as={`/users/${slackId}`}>
-                                        <a title={`${profile.real_name}'s page`} css={linkCss}>
-                                            <Image src={profile.image_48} avatar />
-                                            <span css={{
-                                                display: 'inline-block',
-                                                verticalAlign: 'middle',
-                                                marginLeft: 5,
-                                            }}>
-                                            {profile.real_name}
-                                        </span>
-                                        </a>
-                                    </Link>
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Link href='/users/[id]' as={`/users/${slackId}`}>
-                                        <a title={`${profile.real_name}'s page`} css={linkCss}>
-                                            {count}
-                                            {count === bestIndividualSetCount && (
-                                                <span css={{
-                                                    marginLeft: 10,
-                                                }}>
-                                                    <Icon color="yellow" name="star" />
-                                                </span>
-                                            )}
-                                        </a>
-                                    </Link>
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
-                </Table.Body>
+        <>
+            {activePage === 1 && (
+                <div css={dateDisclaimer}>Date and time displayed are in each participant's local time.</div>
             )}
+            <Table celled padded selectable size='large' striped textAlign="left">
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell className="mobile-hidden" width={1}>Sets
+                            <span css={{color: BLUE, marginLeft: 6}}>
+                                ({totalSets.toLocaleString()})
+                            </span>
+                        </Table.HeaderCell>
+                        <DateDisclaimerTooltip trigger={
+                             <Table.HeaderCell>
+                                 Day
+                             </Table.HeaderCell>
+                         }
+                        />
+                        <DateDisclaimerTooltip trigger={
+                            <Table.HeaderCell>
+                                Date
+                            </Table.HeaderCell>
+                        }
+                        />
+                        <DateDisclaimerTooltip trigger={
+                            <Table.HeaderCell>
+                                Time
+                            </Table.HeaderCell>
+                        }
+                        />
+                        <Table.HeaderCell>Athlete</Table.HeaderCell>
+                        <Table.HeaderCell width={2}>Count
+                            <span css={{color: BLUE, marginLeft: 6}}>
+                                ({totalPushUps.toLocaleString()})
+                            </span>
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
 
-            <Table.Footer>
-                <Table.Row>
-                    <Table.HeaderCell colSpan='6'>
-                        <div css={{
-                            alignItems: 'center',
-                            display: 'flex',
-                            justifyContent: totalPages > 1 ? 'normal': 'flex-end',
-                            '@media(max-width: 767px)': {
-                                flexDirection: 'column',
-                                justifyContent: 'normal',
-                            }
-                        }}>
+                {!data ? <LoadingTableView /> : (
+                    <Table.Body>
+                        {data.globalUsersFeed.feed.map(({slackId, dayOfWeek, date, time, count, simplifiedDate, profile}, i, arr) => {
+                            const rowSpan = data.globalUsersFeed.setsByDayMap[simplifiedDate];
+
+                            const firstIndex = arr.findIndex(item => item.simplifiedDate === simplifiedDate);
+
+                            const shouldCellBeHidden = rowSpan > 1 && i > firstIndex;
+
+                            const maybeSetsCell = shouldCellBeHidden ? null : (
+                                <MaybeLink className="mobile-hidden"
+                                           realName={profile.real_name}
+                                           rowSpan={rowSpan}
+                                           slackId={slackId}>
+                                    {rowSpan}
+                                </MaybeLink>
+                            );
+
+                            const maybeDayCell = (
+                                <MaybeLink rowSpan={rowSpan}
+                                           realName={profile.real_name}
+                                           shouldCellBeHidden={shouldCellBeHidden}
+                                           slackId={slackId}>
+                                    {dayOfWeek}
+                                </MaybeLink>
+                            );
+
+                            const maybeDateCell = (
+                                <MaybeLink rowSpan={rowSpan}
+                                           realName={profile.real_name}
+                                           shouldCellBeHidden={shouldCellBeHidden}
+                                           slackId={slackId}>
+                                    {date}
+                                </MaybeLink>
+                            );
+
+                            return (
+                                <Table.Row key={i}>
+                                    {maybeSetsCell}
+                                    {maybeDayCell}
+                                    {maybeDateCell}
+                                    <Table.Cell>
+                                        <Link href='/users/[id]' as={`/users/${slackId}`}>
+                                            <a title={`${profile.real_name}'s page`} css={linkCss}>
+                                                {time}
+                                            </a>
+                                        </Link>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link href='/users/[id]' as={`/users/${slackId}`}>
+                                            <a title={`${profile.real_name}'s page`} css={linkCss}>
+                                                <Image src={profile.image_48} avatar />
+                                                <span css={{
+                                                    display: 'inline-block',
+                                                    verticalAlign: 'middle',
+                                                    marginLeft: 5,
+                                                }}>
+                                                {profile.real_name}
+                                            </span>
+                                            </a>
+                                        </Link>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link href='/users/[id]' as={`/users/${slackId}`}>
+                                            <a title={`${profile.real_name}'s page`} css={linkCss}>
+                                                {count}
+                                                {count === bestIndividualSetCount && (
+                                                    <span css={{
+                                                        marginLeft: 10,
+                                                    }}>
+                                                        <Icon color="yellow" name="star" />
+                                                    </span>
+                                                )}
+                                            </a>
+                                        </Link>
+                                    </Table.Cell>
+                                </Table.Row>
+                            )
+                        })}
+                    </Table.Body>
+                )}
+
+                <Table.Footer>
+                    <Table.Row>
+                        <Table.HeaderCell colSpan='6'>
                             <div css={{
-                                color: 'rgba(0,0,0,.6)',
+                                alignItems: 'center',
+                                display: 'flex',
+                                justifyContent: totalPages > 1 ? 'normal': 'flex-end',
                                 '@media(max-width: 767px)': {
-                                    textAlign: 'center',
-                                    lineHeight: 1.6,
+                                    flexDirection: 'column',
+                                    justifyContent: 'normal',
                                 }
                             }}>
-                                Use
-                                <code css={{
-                                    backgroundColor: 'rgba(27,31,35,.05)',
-                                    borderRadius: 3,
-                                    fontSize: '95%',
-                                    padding: '.2em .4em',
-                                    marginLeft: 4,
-                                    marginRight: 4,
-                                }}>/pushups
-                                </code>
-                                in <Icon aria-label="Slack" name="slack" size="large" />to log sets and
-                                <code css={{
-                                    backgroundColor: 'rgba(27,31,35,.05)',
-                                    borderRadius: 3,
-                                    fontSize: '95%',
-                                    padding: '.2em .4em',
-                                    marginLeft: 4,
-                                    marginRight: 4,
-                                }}>/challenge
-                                </code>
-                                to challenge someone
-                            </div>
-                            {totalPages > 1 && (
                                 <div css={{
-                                    marginLeft: 'auto',
+                                    color: 'rgba(0,0,0,.6)',
                                     '@media(max-width: 767px)': {
-                                        marginLeft: 'unset',
-                                        marginTop: 30
+                                        textAlign: 'center',
+                                        lineHeight: 1.6,
                                     }
                                 }}>
-                                    <FeedPagination
-                                        activePage={activePage}
-                                        onPageChange={(e, {activePage}) => {
-                                            setActivePage(activePage);
-                                        }}
-                                        totalPages={totalPages}
-                                    />
+                                    Use
+                                    <code css={{
+                                        backgroundColor: 'rgba(27,31,35,.05)',
+                                        borderRadius: 3,
+                                        fontSize: '95%',
+                                        padding: '.2em .4em',
+                                        marginLeft: 4,
+                                        marginRight: 4,
+                                    }}>/pushups
+                                    </code>
+                                    in <Icon aria-label="Slack" name="slack" size="large" />to log sets and
+                                    <code css={{
+                                        backgroundColor: 'rgba(27,31,35,.05)',
+                                        borderRadius: 3,
+                                        fontSize: '95%',
+                                        padding: '.2em .4em',
+                                        marginLeft: 4,
+                                        marginRight: 4,
+                                    }}>/challenge
+                                    </code>
+                                    to challenge someone
                                 </div>
-                            )}
-                        </div>
-                    </Table.HeaderCell>
-                </Table.Row>
-            </Table.Footer>
-        </Table>
+                                {totalPages > 1 && (
+                                    <div css={{
+                                        marginLeft: 'auto',
+                                        '@media(max-width: 767px)': {
+                                            marginLeft: 'unset',
+                                            marginTop: 30
+                                        }
+                                    }}>
+                                        <FeedPagination
+                                            activePage={activePage}
+                                            onPageChange={(e, {activePage}) => {
+                                                setActivePage(activePage);
+                                            }}
+                                            totalPages={totalPages}
+                                        />
+                                    </div>
+                                )}
+                            </div>
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Footer>
+            </Table>
+        </>
     )
 };
 
