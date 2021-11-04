39e3fc8b079e617bfc7c929c89f59694ca3c3575
Joanne Daudier <joanne.daudier@zapier.com>
2021-11-04 14:43:53 -0700

    Change max num for summary to 30

--- utils/constants.js
+++ utils/constants.js
@@ -3,5 +3,5 @@ export const RED = '#dd2e44';
 export const YELLOW = '#ffac33';
 
 export const FEED_LIMIT = 50;
-export const MAX_NUM_FOR_SUMMARY = 20;
+export const MAX_NUM_FOR_SUMMARY = 30;
 export const NUM_HOURS_AWAKE = 10;
